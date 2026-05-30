2d91ffbe80d21b6ef5c715e078f4333ae33fb3e7
Pratham Srivastava <prathamsri04@gmail.com>
2026-05-30 18:44:35 +0530

    fix: remove custom theme config on RootProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,13 +12,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider
-          theme={{
-            defaultTheme: "system",
-            enableSystem: true,
-            attribute: "class",
-          }}
-        >
+        <RootProvider>
           <BookmarkProvider>{children}</BookmarkProvider>
         </RootProvider>
       </body>
